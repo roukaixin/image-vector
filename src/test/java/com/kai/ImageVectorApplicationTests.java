4cdde86d3b57c12587fa90b1434929b7f9bfc07a
pankx <a3427173515@163.com>
2023-03-30 20:10:55 +0800

    加密敏感数据

--- src/test/java/com/kai/ImageVectorApplicationTests.java
+++ src/test/java/com/kai/ImageVectorApplicationTests.java
@@ -1,16 +1,27 @@
 package com.kai;
 
-import com.kai.utlis.GiteeUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ImageVectorApplication.class)
+@Slf4j
 class ImageVectorApplicationTests {
+
+    private final StringEncryptor stringEncryptor;
+
+    @Autowired
+    ImageVectorApplicationTests(StringEncryptor stringEncryptor) {
+        this.stringEncryptor = stringEncryptor;
+    }
 
     @Test
     void contextLoads() {
-        String newFile = GiteeUtil.createNewFile(null, null, null);
-
+        // 加密
+        String encrypt = stringEncryptor.encrypt("123456");
+        log.info(encrypt);
     }
 
 }
